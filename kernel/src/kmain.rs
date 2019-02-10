c9a8513acd08499696b93dd9b1a2afd2bdcccc98
jouny <dz4tune@gmail.com>
2019-02-10 19:48:19 +0300

    timer driver

--- kernel/src/kmain.rs
+++ kernel/src/kmain.rs
@@ -8,9 +8,19 @@
 #![feature(attr_literals)]
 #![feature(never_type)]
 #![feature(ptr_internals)]
+#![feature(pointer_methods)]
 
 extern crate pi;
 extern crate stack_vec;
+
+use pi::timer::*;
+
+const GPIO_BASE: usize = 0x3F000000 + 0x200000;
+
+const GPIO_FSEL1: *mut u32 = (GPIO_BASE + 0x04) as *mut u32;
+const GPIO_SET0: *mut u32 = (GPIO_BASE + 0x1C) as *mut u32;
+const GPIO_CLR0: *mut u32 = (GPIO_BASE + 0x28) as *mut u32;
+
 
 pub mod lang_items;
 pub mod mutex;
@@ -18,6 +28,17 @@ pub mod console;
 pub mod shell;
 
 #[no_mangle]
-pub extern "C" fn kmain() {
+pub unsafe extern "C" fn kmain() {
+    // STEP 1: Set GPIO Pin 16 as output.
+    GPIO_FSEL1.write_volatile(0b001 << 18);
+    // STEP 2: Continuously set and clear GPIO 16.
+    let gpio16: u32 = 0b1 << 16;
+    loop {
+        GPIO_SET0.write_volatile(gpio16);
+        spin_sleep_ms(1000);
+        GPIO_CLR0.write_volatile(gpio16);
+        spin_sleep_ms(500);
+    }
+
     // FIXME: Start the shell.
 }
--- pi/src/timer.rs
+++ pi/src/timer.rs
@@ -7,44 +7,50 @@ const TIMER_REG_BASE: usize = IO_BASE + 0x3000;
 
 #[repr(C)]
 #[allow(non_snake_case)]
-struct Registers {
-    CS: Volatile<u32>,
-    CLO: ReadVolatile<u32>,
-    CHI: ReadVolatile<u32>,
-    COMPARE: [Volatile<u32>; 4]
+struct TimerRegisters {
+    control_status: Volatile<u32>,
+    counter_lower: ReadVolatile<u32>,
+    counter_higher: ReadVolatile<u32>,
+    compare: [Volatile<u32>; 4]
 }
 
 /// The Raspberry Pi ARM system timer.
 pub struct Timer {
-    registers: &'static mut Registers
+    registers: &'static mut TimerRegisters
 }
 
 impl Timer {
     /// Returns a new instance of `Timer`.
     pub fn new() -> Timer {
         Timer {
-            registers: unsafe { &mut *(TIMER_REG_BASE as *mut Registers) },
+            registers: unsafe { &mut *(TIMER_REG_BASE as *mut TimerRegisters) },
         }
     }
 
     /// Reads the system timer's counter and returns the 64-bit counter value.
     /// The returned value is the number of elapsed microseconds.
     pub fn read(&self) -> u64 {
-        unimplemented!()
+        let mut result: u64 = (self.registers.counter_higher.read() as u64) << 32;
+        result | self.registers.counter_lower.read() as u64
     }
 }
 
 /// Returns the current time in microseconds.
 pub fn current_time() -> u64 {
-    unimplemented!()
+    Timer::new().read()
 }
 
 /// Spins until `us` microseconds have passed.
 pub fn spin_sleep_us(us: u64) {
-    unimplemented!()
+    let timer = Timer::new();
+    let now = timer.read() + us;
+    timer.registers.compare[0].write(now as u32);
+    while timer.registers.control_status.read() & 1 == 0 {
+        
+    }
 }
 
 /// Spins until `ms` milliseconds have passed.
 pub fn spin_sleep_ms(ms: u64) {
-    unimplemented!()
+    spin_sleep_us(ms*1000);
 }
